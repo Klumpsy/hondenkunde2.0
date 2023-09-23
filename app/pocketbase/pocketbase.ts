206a4c98b279e9b4d3cd0421046bbbbfe8d85221
bart <bart_klumperman@Live.nl>
2023-09-23 14:56:33 +0200

    new

--- app/pocketbase/pocketbase.ts
+++ app/pocketbase/pocketbase.ts
@@ -1,5 +1,12 @@
 import PocketBase from 'pocketbase'
 
+export const dynamic = 'auto',
+dynamicParams = true,
+revalidate = 0,
+fetchCache = 'auto',
+runtime = 'nodejs',
+preferredRegion = 'auto'
+
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 export const getBlogs = async () => {
