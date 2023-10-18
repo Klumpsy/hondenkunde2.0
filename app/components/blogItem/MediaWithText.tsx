f30d6829ae5c046c0ff4388b702a06724b0a7f6f
bart <bart_klumperman@Live.nl>
2023-10-18 20:11:37 +0200

    yes

--- app/components/blogItem/MediaWithText.tsx
+++ app/components/blogItem/MediaWithText.tsx
@@ -18,7 +18,7 @@ const MediaWithText: React.FC<MediaWithTextProps> = ({
   title,
   isVideo,
 }) => {
-  function extractVideoID(url) {
+  function extractVideoID(url: string) {
     const videoIDRegex =
       /(?:www\.youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
     const matches = url.match(videoIDRegex);
