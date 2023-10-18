a24d88705445d0f9d4d5a96c417d6f363bc14f48
bart <bart_klumperman@Live.nl>
2023-10-18 17:56:36 +0200

    styling fixed

--- app/artiRating/[slug]/page.tsx
+++ app/artiRating/[slug]/page.tsx
@@ -1,4 +1,9 @@
-import { getFileUrlRatingItem, getSingleRatingItem, getFileUrlsForProductImages, getRatingItems } from "@/app/pocketbase/pocketbase";
+import {
+  getFileUrlRatingItem,
+  getSingleRatingItem,
+  getFileUrlsForProductImages,
+  getRatingItems,
+} from "@/app/pocketbase/pocketbase";
 import Link from "next/link";
 import Image from "next/image";
 import RatingBone from "@/app/components/ratingCard/RatingBone";
@@ -6,100 +11,137 @@ import Slider from "@/app/components/slider/Slider";
 import { extractVideoID } from "@/app/helpers/videoHelper";
 
 const RatingDetail = async ({ params }: any) => {
+  const ratingDetail = await getSingleRatingItem(params.slug);
+  const coverImageUrl = await getFileUrlRatingItem(ratingDetail, "coverImage");
+  const urls = await getFileUrlsForProductImages(ratingDetail);
 
-    const ratingDetail = await getSingleRatingItem(params.slug);
-    const coverImageUrl = await getFileUrlRatingItem(ratingDetail, 'coverImage');
-    const urls = await getFileUrlsForProductImages(ratingDetail);
-
-    return (
-        <div className="bg-gray-900 py-8 pt-12 min-h-screen">
-            <Link href="/artiRating">
-                 <button className="hover:bg-orange hover:text-darkBlue font-extrabold right-0 md:left-0 md:right-auto top-0 m-4 bg-gray-700 text-orange py-2 px-4 rounded-full shadow-md transition">
-                     Terug naar ratings
-                </button>
-            </Link>
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+  return (
+    <div className="bg-gray-900 py-8 pt-12 min-h-screen">
+      <Link href="/artiRating">
+        <button className="hover:bg-orange hover:text-darkBlue font-extrabold right-0 md:left-0 md:right-auto top-0 m-4 bg-gray-700 text-orange py-2 px-4 rounded-full shadow-md transition">
+          Terug naar ratings
+        </button>
+      </Link>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row -mx-4">
-            <div className="md:flex-1 px-4 z-1">
+          <div className="md:flex-1 px-4 z-1">
             <div className="relative h-[460px] rounded-lg bg-gray-300 mb-4 overflow-hidden">
-                <Image 
-                    src={coverImageUrl} 
-                    alt="Product Image" 
-                    layout="fill" 
-                    objectFit="cover" 
-                    className="absolute" />
+              <Image
+                src={coverImageUrl}
+                alt="Product Image"
+                layout="fill"
+                objectFit="cover"
+                className="absolute"
+              />
+            </div>
+          </div>
+          <div className="md:flex-1 px-4 bg-gray-900 p-4">
+            <h2 className="text-2xl font-bold mb-2 text-orange">
+              {ratingDetail.title}
+            </h2>
+            <div className="mb-4">
+              <span className="font-bold text-gray-700 text-white">
+                Beoordeeld door{" "}
+                <span className="text-orange">{ratingDetail.ratedBy} </span>:
+              </span>
+              <div className="flex items-center mt-2 text-white">
+                <RatingBone
+                  rating={ratingDetail.rating}
+                  ratedBy={ratingDetail.ratedBy}
+                />
+              </div>
+            </div>
+            <div className="mb-4">
+              <div className="flex space-x-2">
+                {ratingDetail.tags &&
+                  ratingDetail.tags.map((tag) => (
+                    <p
+                      key={tag + ratingDetail.id + ratingDetail.buttonUrl}
+                      className={`flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3 rounded-full uppercase inline-block ${tag}`}
+                    >
+                      {tag}
+                    </p>
+                  ))}
+              </div>
+            </div>
+            <div>
+              <span className="font-bold text-gray-700 text-white">
+                Over dit product:
+              </span>
+              <p
+                className="text-gray-600 text-sm mt-2 text-white"
+                dangerouslySetInnerHTML={{
+                  __html: ratingDetail.explanationText,
+                }}
+              />
+            </div>
+            <div
+              className={`mt-4 flex flex-col md:flex-row ${
+                ratingDetail.blogUrl ? "-mx-2" : "justify-center"
+              } mb-4`}
+            >
+              {ratingDetail.blogUrl && (
+                <div className="mb-2 md:mb-0 md:w-1/2 px-2">
+                  <Link href={ratingDetail.blogUrl}>
+                    <button className="w-full bg-darkBlue text-orange py-2 px-4 rounded-full font-bold hover:bg-gray-800">
+                      {ratingDetail.blogButtonText}
+                    </button>
+                  </Link>
                 </div>
-                <div className={`flex ${ratingDetail.blogUrl ? '-mx-2' : 'justify-center'} mb-4`}>
-                {ratingDetail.blogUrl && (
-                    <div className="w-1/2 px-2">
-                       <Link href={ratingDetail.blogUrl}>
-                            <button className="w-full bg-darkBlue text-orange py-2 px-4 rounded-full font-bold hover:bg-gray-800">
-                                {ratingDetail.blogButtonText}
-                            </button>
-                        </Link>
-                    </div>
-                )}
-                <div className={`${ratingDetail.blogUrl ? 'w-1/2' : 'w-full max-w-xs'} px-2`}>
+              )}
+              <div
+                className={`${
+                  ratingDetail.blogUrl ? "md:w-1/2" : "w-full max-w-xs"
+                } px-2`}
+              >
                 {ratingDetail.buttonUrl && (
-                    <Link href={ratingDetail.buttonUrl} target="_blank">
-                        <button className="w-full bg-orange text-darkBlue py-2 px-4 rounded-full font-bold hover:bg-gray-800 hover:text-orange">
-                             {ratingDetail.buttonText}
-                        </button>
-                    </Link>
+                  <Link href={ratingDetail.buttonUrl} target="_blank">
+                    <button className="w-full bg-green-400 text-darkBlue py-2 px-4 rounded-full font-bold hover:bg-green-500 hover:text-white">
+                      {ratingDetail.buttonText}
+                    </button>
+                  </Link>
                 )}
-                </div>
+              </div>
             </div>
-            </div>
-            <div className="md:flex-1 px-4 bg-gray-900 p-4">
-                <h2 className="text-2xl font-bold mb-2 text-orange">{ratingDetail.title}</h2>
-                <div className="mb-4">
-                    <span className="font-bold text-gray-700 text-white">Beoordeeld door <span className='text-orange'>{ratingDetail.ratedBy} </span>:</span>
-                    <div className="flex items-center mt-2 text-white">
-                        <RatingBone rating={ratingDetail.rating} ratedBy={ratingDetail.ratedBy}/>
-                    </div>
-                </div>
-                <div className="mb-4">
-                    <div className="flex space-x-2">
-                            {ratingDetail.tags && ratingDetail.tags.map(tag => (
-                             <p key={tag + ratingDetail.id + ratingDetail.buttonUrl} 
-                                 className={`flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3 rounded-full uppercase inline-block ${tag}`}>
-                                  {tag}
-                                </p>
-                            ))}
-                    </div>
-                    </div>
-                    <div>
-                        <span className="font-bold text-gray-700 text-white">Over dit product:</span>
-                        <p className="text-gray-600 text-sm mt-2 text-white" dangerouslySetInnerHTML={{ __html: ratingDetail.explanationText}} />
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-        <Slider imagePaths={urls}/>
-        {ratingDetail.productVideo && ( 
-            <div className="mt-7 max-w-[1000px] mx-auto w-full tablet:max-w-[90%] mobile:max-w-[90%]">
-                <h3 className="mb-2 text-orange text-2xl font-extrabold">{ratingDetail.ratedBy} aan het </h3>
-                <div className="relative" style={{paddingTop: '56.25%'}}> {/* This padding percentage represents a 16:9 aspect ratio */}
-                    <iframe 
-                        className="absolute top-0 left-0 w-full h-full"
-                        src={`https://www.youtube.com/embed/${extractVideoID(ratingDetail.productVideo)}`} 
-                        frameBorder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowFullScreen
-                    />
-                </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="flex flex-wrap justify-between max-w-[1100px] w-full">
+          <div className="mt-7 w-full md:w-1/2 p-3">
+            <Slider imagePaths={urls} />
+          </div>
+
+          {ratingDetail.productVideo && (
+            <div className="mt-7 w-full md:w-1/2 p-3">
+              <h3 className="mb-2 mt-4 text-orange text-2xl font-extrabold">
+                {ratingDetail.ratedBy} in actie:
+              </h3>
+              <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
+                <iframe
+                  className="absolute top-0 left-0 w-full h-full"
+                  src={`https://www.youtube.com/embed/${extractVideoID(
+                    ratingDetail.productVideo
+                  )}`}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                />
+              </div>
             </div>
-        )}
+          )}
+        </div>
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default RatingDetail;
 
 export async function generateStaticParams() {
-    const ratingItems = await getRatingItems();
-    
-    return ratingItems.map((ratingItem) => ({
-        slug: ratingItem.slug,
-    }))
+  const ratingItems = await getRatingItems();
+
+  return ratingItems.map((ratingItem) => ({
+    slug: ratingItem.slug,
+  }));
 }
--- app/components/blogItem/MediaWithText.tsx
+++ app/components/blogItem/MediaWithText.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 interface MediaWithTextProps {
   mediaSrc: string;
@@ -10,47 +10,58 @@ interface MediaWithTextProps {
   isVideo?: boolean;
 }
 
-const MediaWithText: React.FC<MediaWithTextProps> = (
-  { mediaSrc, 
-    mediaAlt, 
-    text, 
-    orientation, 
-    title, 
-    isVideo }) => {
+const MediaWithText: React.FC<MediaWithTextProps> = ({
+  mediaSrc,
+  mediaAlt,
+  text,
+  orientation,
+  title,
+  isVideo,
+}) => {
+  function extractVideoID(url) {
+    const videoIDRegex =
+      /(?:www\.youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const matches = url.match(videoIDRegex);
+    return matches ? matches[1] : null;
+  }
 
-      function extractVideoID(url) {
-        const videoIDRegex = /(?:www\.youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-        const matches = url.match(videoIDRegex);
-        return matches ? matches[1] : null;
-      }
-  
   return (
     <section className="p-4 space-y-4">
       <h2 className="text-2xl font-bold mb-3">{title}</h2>
-      <div className={`flex flex-col md:flex-row ${orientation === 'right' ? 'md:flex-row-reverse' : ''} items-center space-y-4 md:space-y-0 md:space-x-4`}>
+      <div
+        className={`xl:flex ${
+          orientation === "right" ? "xl:flex-row-reverse" : ""
+        } items-center space-y-4 xl:space-y-0 xl:space-x-4`}
+      >
         {mediaSrc && (
-          <div className="flex-shrink-0 w-full md:w-auto m-4">
-            {isVideo ? ( 
-             <div className="responsive-video-wrapper">
-                <iframe 
-                  src={`https://www.youtube.com/embed/${extractVideoID(mediaSrc)}`} 
-                  frameBorder="0" 
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          <div className="m-4 md:float-left md:w-1/3 lg:w-1/4 xl:float-none xl:flex-shrink-0 xl:w-auto">
+            {isVideo ? (
+              <div className="responsive-video-wrapper">
+                <iframe
+                  src={`https://www.youtube.com/embed/${extractVideoID(
+                    mediaSrc
+                  )}`}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                 />
-            </div>
+              </div>
             ) : (
-              <Image 
-                src={mediaSrc} 
-                alt={mediaAlt} 
-                width={300} 
-                height={200} 
-                className="rounded-lg object-cover w-full" />
+              <Image
+                src={mediaSrc}
+                alt={mediaAlt}
+                width={300}
+                height={200}
+                className="rounded-lg object-cover w-full"
+              />
             )}
           </div>
         )}
-        <div className="flex-grow space-y-2">
-          <div className="text-gray-700 leading-relaxed" dangerouslySetInnerHTML={{ __html: text }} />
+        <div className="space-y-2">
+          <div
+            className="text-gray-700 leading-relaxed"
+            dangerouslySetInnerHTML={{ __html: text }}
+          />
         </div>
       </div>
     </section>
