142f56acf1829a56b7d0ca0a56a7038c7c34e31e
bart <bart_klumperman@Live.nl>
2024-09-29 16:42:28 +0200

    Searchbar implemented

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,18 @@
 import BlogItem from "../components/blogItem/BlogItem";
 import { getBlogs } from "../pocketbase/pocketbase";
 import Header from "../components/header/Header";
+import SearchBar from "../components/filter/SearchBar";
 
-const Blog = async () => {
-  const blogs = await getBlogs();
+const Blog = async ({
+  searchParams,
+}: {
+  searchParams: { search?: string; tags?: string };
+}) => {
+  const search = searchParams?.search || "";
+  const tagsParam = searchParams?.tags || "";
+  const selectedTags = tagsParam ? tagsParam.split(",") : [];
+
+  const blogs = await getBlogs(search, selectedTags);
 
   return (
     <div>
@@ -14,17 +23,17 @@ const Blog = async () => {
         anchorText="Bekijk Arti's rating"
       />
       <section className="paw-pattern max-w-[1200px] mx-auto"></section>
-
+      <div className="relative flex w-full justify-center space-x-5">
+        <SearchBar baseRoute="/blog" placeholder="Zoek in blogs..." />
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-[1200px] mx-auto p-3">
-        {blogs?.map((blogItem, index) => {
-          return (
-            <BlogItem
-              key={blogItem.id}
-              blogItem={blogItem}
-              className="col-span-1 md:col-span-1 lg:col-span-1"
-            />
-          );
-        })}
+        {blogs?.map((blogItem) => (
+          <BlogItem
+            key={blogItem.id}
+            blogItem={blogItem}
+            className="col-span-1 md:col-span-1 lg:col-span-1"
+          />
+        ))}
       </div>
     </div>
   );
--- app/pocketbase/pocketbase.ts
+++ app/pocketbase/pocketbase.ts
@@ -2,38 +2,74 @@ import PocketBase from 'pocketbase'
 
 const pb = new PocketBase(`${process.env.NEXT_DB_BASE_URL}`);
 
-export const getBlogs = async () => {
+export const getBlogs = async (search?: string, tags?: string[]) => {
+  const page = 1;
+  const limit = 100;
 
-const page = 1;
-const limit = 100;
+  let filter = '';
+  
+  // Add tag filters to the query if tags are present
+  if (tags && tags.length > 0) {
+    const tagFilter = tags.map(tag => `tags~'${tag}'`).join(' && ');
+    filter += `(${tagFilter})`;
+  }
 
-let url = `${process.env.NEXT_DB_BASE_URL}/api/collections/blogs/records?page=${page}&perPage=${limit}&sort=-created`
+  console.log(search);
 
-  const res = await fetch(url, { 
+  if (search) {
+    console.log(search)
+    const searchFilter = `(title~'${search}' || introText~'${search}')`;
+    filter += filter ? ` && ${searchFilter}` : searchFilter;
+  }
+
+  let url = `${process.env.NEXT_DB_BASE_URL}/api/collections/blogs/records?page=${page}&perPage=${limit}&sort=-created`;
+
+  // Append the filter to the URL if any filter is present
+  if (filter) {
+    url += `&filter=${encodeURIComponent(filter)}`;
+  }
+
+  const res = await fetch(url, {
     next: {
       revalidate: 10,
-     },
-   });
+    },
+  });
+  
   const data = await res.json();
-
   return data?.items as any[];
-}
+};
 
-export const getRatingItems = async () => {
+export const getRatingItems = async (search?: string, tags?: string[]) => {
+  const page = 1;
+  const limit = 100;
 
-    const res = await fetch(
-        `${process.env.NEXT_DB_BASE_URL}/api/collections/ratingItems/records?page=1&perPage=100`, 
-        { 
-          next: {
-            revalidate: 10,
-           },
-        }
-    );
-    const data = await res.json();
+  let filter = '';
 
+  if (tags && tags.length > 0) {
+    const tagFilter = tags.map(tag => `tags~'${tag}'`).join(' && ');
+    filter += `(${tagFilter})`;
+  }
 
-    return data?.items as any[];
-}
+  if (search) {
+    const searchFilter = `(title~'${search}' || explanationText~'${search}')`;
+    filter += filter ? ` && ${searchFilter}` : searchFilter;
+  }
+
+  let url = `${process.env.NEXT_DB_BASE_URL}/api/collections/ratingItems/records?page=${page}&perPage=${limit}&sort=-created`;
+
+  if (filter) {
+    url += `&filter=${encodeURIComponent(filter)}`;
+  }
+
+  const res = await fetch(url, {
+    next: {
+      revalidate: 10,
+    },
+  });
+  
+  const data = await res.json();
+  return data?.items as any[];
+};
 
 
 export const getSingleBlog = async (slug: string) => {
